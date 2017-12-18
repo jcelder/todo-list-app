6904a87f460ad74e73da9c21304d69779c4ea3f1
Josh <aresx130@gmail.com>
2017-12-18 14:40:08 -0800

    Set up postgres db connection

---
+++ src/models/db/connection.js
@@ -0,0 +1,7 @@
+const pgp = require('pg-promise')()
+
+const connection = process.env.NODE_ENV === 'test' ? 'postgres:///todolist_test' : 'postgres:///todolist'
+
+const db = pgp(connection)
+
+module.exports = { db }
