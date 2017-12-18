95bcda11f2d27fdb298f452e34ae1eb6a810817c
Josh <aresx130@gmail.com>
2017-12-18 12:15:41 -0800

    Add express route handler boilerplate

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import session from 'express-session'
+import router from './server/routes/index'
 
 const app = express()
 
@@ -20,6 +21,8 @@ app.set('views', `${__dirname}/views`)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(session(sess))
+
+app.use('/', router)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
