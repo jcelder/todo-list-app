031adf2ebf1e5f8bf2870246dd11730a2c5b55d8
Josh <aresx130@gmail.com>
2017-12-18 15:05:08 -0800

    Add boilerplate for 'tasks' db functions integration testing

---
+++ test/integration/tasks.test.js
@@ -0,0 +1,8 @@
+/* eslint-env node, mocha */
+const { expect } = require('chai')
+const db = require('../../src/models/db/connection')
+const { resetTasks } = require('../helpers/db/tasks.db')
+
+describe('Tasks DB Functions Integration Tests', () => {
+  
+})
