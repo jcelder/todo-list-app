4d2eca96fcf943fff5d89ad984b96e7e276ab62a
Josh <aresx130@gmail.com>
2017-12-21 15:28:08 -0800

    Add 'tasks' routes

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
+const tasks = require('../../models/tasks')
+const tasksRoutes = require('./tasks')
 
 router.get('/', (req, res) => {
-  res.send('Test page, please ignore')
+  res.redirect('/tasks')
 })
+
+router.use('/', tasksRoutes)
 
 module.exports = router
