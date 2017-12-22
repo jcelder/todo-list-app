8487807a487d90376bdf52e7f826c12b84e53a14
Josh <aresx130@gmail.com>
2017-12-22 15:40:33 -0800

    Set up bodyParser to parse JSON

--- src/server.js
+++ src/server.js
@@ -19,6 +19,7 @@ app.set('view engine', 'pug')
 app.set('views', `${__dirname}/views`)
 
 app.use(express.static('public'))
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(session(sess))
 
